aa8b98dbe30a949edad643ab157cb4346abb66c6
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2020-05-28 18:41:22 +0530

    conditional class

--- project1/src/components/TodoItem.vue
+++ project1/src/components/TodoItem.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}
+  ">
     <p>{{todo.title}}</p>
   </div>
 </template>
 <script>
 export default {
   name: "TodoItem",
-  props:["todo"]
+  props: ["todo"]
 };
 </script>
 
